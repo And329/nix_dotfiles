e0a548c3233de6130a038abf9c4ace90a2e72bdc
And329 <329@riseup.net>
2025-08-02 21:12:54 +0300

    Fix i3status-rust config: move to home-manager, clean up modules

--- home/zen.nix
+++ home/zen.nix
@@ -11,5 +11,30 @@
     userEmail = "329@riseup.net";
   };
 
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      default = {
+        blocks = [
+          {
+            block = "disk_space";
+            path = "/";
+            info_type = "available";
+            interval = 60;
+            warning = 20.0;
+            alert = 10.0;
+          }
+          { block = "memory"; }
+          { block = "cpu"; interval = 1; }
+          { block = "load"; interval = 1; }
+          { block = "time"; interval = 60; format = "%a %d/%m %R"; }
+        ];
+        settings = {
+          theme = "solarized-dark";
+        };
+      };
+    };
+  };
+
   home.stateVersion = "25.05";
 }
--- modules/i3.nix
+++ modules/i3.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Enable i3 window manager
   services.xserver.windowManager.i3 = {
     enable = true;
     package = pkgs.i3-gaps;
@@ -15,37 +14,9 @@
     ];
   };
 
-  # Configure i3status-rust
-  programs.i3status-rust = {
-    enable = true;
-    bars.default = {
-      blocks = [
-        {
-          block = "disk_space";
-          path = "/";
-          info_type = "available";
-          interval = 60;
-          warning = 20.0;
-          alert = 10.0;
-        }
-        { block = "memory"; }
-        { block = "cpu"; interval = 1; }
-        { block = "load"; interval = 1; }
-        { block = "time"; interval = 60; format = "%a %d/%m %R"; }
-      ];
-      settings = {
-        theme = "solarized-dark";
-      };
-    };
-  };
-
-  # Set i3 as the default window manager
   services.xserver.displayManager.defaultSession = "none+i3";
-
-  # Enable lightdm display manager
   services.xserver.displayManager.lightdm.enable = true;
 
-  # Add i3 and related packages to system packages
   environment.systemPackages = with pkgs; [
     i3-gaps
     i3status-rust
@@ -59,10 +30,7 @@
     blueman
   ];
 
-  # Enable NetworkManager applet
   programs.nm-applet.enable = true;
-
-  # Enable bluetooth support
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 }
